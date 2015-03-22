2355ffbe1ea9555017eeb651dba86722ca8b61bd
jeroenpeters1986 <jeroenpeters1986@gmail.com>
2015-03-22 16:59:00 +0100

    Barcodescanner

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/jeroen/PhpstormProjects/mi_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/jeroen/PhpstormProjects/mi_app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/jeroen/PhpstormProjects/mi_app/platforms/android/ant-build/AndroidManifest.xml : \
+/home/jeroen/PhpstormProjects/mi_app/platforms/android/AndroidManifest.xml \
